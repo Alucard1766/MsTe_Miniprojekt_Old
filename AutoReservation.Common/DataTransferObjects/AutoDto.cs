9ea0d6a9297ab3c269702a0184c102e9f67a0be5
Luca Salzani <Alucard1766@users.noreply.github.com>
2016-12-08 13:36:40 +0100

    DTO 3.2.1 Done

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -1,33 +1,127 @@
 ﻿using AutoReservation.Common.DataTransferObjects.Core;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class AutoDto //: DtoBase<AutoDto>
+    [DataContract]
+    public class AutoDto : DtoBase<AutoDto>
     {
+        private int basistarif;
+        [DataMember]
+        public int Basistarif
+        {
+            get { return basistarif; }
+            set
+            {
+                if (basistarif != value)
+                {
+                    basistarif = value;
+                    OnPropertyChanged(nameof(Basistarif));
+                }
+            }
+        }
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(marke))
-        //    {
-        //        error.AppendLine("- Marke ist nicht gesetzt.");
-        //    }
-        //    if (tagestarif <= 0)
-        //    {
-        //        error.AppendLine("- Tagestarif muss grösser als 0 sein.");
-        //    }
-        //    if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
-        //    {
-        //        error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
-        //    }
+        private int id;
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        private string marke;
+        [DataMember]
+        public string Marke
+        {
+            get { return marke; }
+            set
+            {
+                if (marke != value)
+                {
+                    marke = value;
+                    OnPropertyChanged(nameof(Marke));
+                }
+            }
+        }
 
-        //    return error.ToString();
-        //}
+        private byte[] rowVersion;
+        [DataMember]
+        public byte[] RowVersion
+        {
+            get { return rowVersion; }
+            set
+            {
+                if (rowVersion != value)
+                {
+                    rowVersion = value;
+                    OnPropertyChanged(nameof(RowVersion));
+                }
+            }
+        }
 
-        //public override string ToString()
-        //    => $"{Id}; {Marke}; {Tagestarif}; {Basistarif}; {AutoKlasse}";
+        private int tagestarif;
+        [DataMember]
+        public int Tagestarif
+        {
+            get { return tagestarif; }
+            set
+            {
+                if (tagestarif != value)
+                {
+                    tagestarif = value;
+                    OnPropertyChanged(nameof(Tagestarif));
+                }
+            }
+        }
+
+        private AutoKlasse autoKlasse;
+        [DataMember]
+        public AutoKlasse AutoKlasse
+        {
+            get { return autoKlasse; }
+            set
+            {
+                if (autoKlasse != value)
+                {
+                    autoKlasse = value;
+                    OnPropertyChanged(nameof(AutoKlasse));
+                }
+            }
+        }
+
+
+
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(marke))
+            {
+                error.AppendLine("- Marke ist nicht gesetzt.");
+            }
+            if (tagestarif <= 0)
+            {
+                error.AppendLine("- Tagestarif muss grösser als 0 sein.");
+            }
+            if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
+            {
+                error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override string ToString() => $"{Id}; {Marke}; {Tagestarif}; {Basistarif}; {AutoKlasse}";
 
     }
 }
--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,57 +1,152 @@
 ﻿using AutoReservation.Common.DataTransferObjects.Core;
+using System;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class ReservationDto //: DtoBase<ReservationDto>
+    [DataContract]
+    public class ReservationDto : DtoBase<ReservationDto>
     {
+        private DateTime bis;
+        [DataMember]
+        public DateTime Bis
+        {
+            get { return bis; }
+            set
+            {
+                if (bis != value)
+                {
+                    bis = value;
+                    OnPropertyChanged(nameof(Bis));
+                }
+            }
+        }
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (Von == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Von-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Bis == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Bis-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Von > Bis)
-        //    {
-        //        error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
-        //    }
-        //    if (Auto == null)
-        //    {
-        //        error.AppendLine("- Auto ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string autoError = Auto.Validate();
-        //        if (!string.IsNullOrEmpty(autoError))
-        //        {
-        //            error.AppendLine(autoError);
-        //        }
-        //    }
-        //    if (Kunde == null)
-        //    {
-        //        error.AppendLine("- Kunde ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string kundeError = Kunde.Validate();
-        //        if (!string.IsNullOrEmpty(kundeError))
-        //        {
-        //            error.AppendLine(kundeError);
-        //        }
-        //    }
+        private int reservationsNr;
+        [DataMember]
+        public int ReservationsNr
+        {
+            get { return reservationsNr; }
+            set
+            {
+                if (reservationsNr != value)
+                {
+                    reservationsNr = value;
+                    OnPropertyChanged(nameof(ReservationsNr));
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        private byte[] rowVersion;
+        [DataMember]
+        public byte[] RowVersion
+        {
+            get { return rowVersion; }
+            set
+            {
+                if (rowVersion != value)
+                {
+                    rowVersion = value;
+                    OnPropertyChanged(nameof(RowVersion));
+                }
+            }
+        }
 
-        //    return error.ToString();
-        //}
+        private DateTime von;
+        [DataMember]
+        public DateTime Von
+        {
+            get { return von; }
+            set
+            {
+                if (von != value)
+                {
+                    von = value;
+                    OnPropertyChanged(nameof(Von));
+                }
+            }
+        }
 
-        //public override string ToString()
-        //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
+        private AutoDto auto;
+        [DataMember]
+        public AutoDto Auto
+        {
+            get { return auto; }
+            set
+            {
+                if (auto != value)
+                {
+                    auto = value;
+                    OnPropertyChanged(nameof(Auto));
+                }
+            }
+        }
+
+        private KundeDto kunde;
+        [DataMember]
+        public KundeDto Kunde
+        {
+            get { return kunde; }
+            set
+            {
+                if (kunde != value)
+                {
+                    kunde = value;
+                    OnPropertyChanged(nameof(Kunde));
+                }
+            }
+        }
+
+
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (Von == DateTime.MinValue)
+            {
+                error.AppendLine("- Von-Datum ist nicht gesetzt.");
+            }
+            if (Bis == DateTime.MinValue)
+            {
+                error.AppendLine("- Bis-Datum ist nicht gesetzt.");
+            }
+            if (Von > Bis)
+            {
+                error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
+            }
+            if (Auto == null)
+            {
+                error.AppendLine("- Auto ist nicht zugewiesen.");
+            }
+            else
+            {
+                string autoError = Auto.Validate();
+                if (!string.IsNullOrEmpty(autoError))
+                {
+                    error.AppendLine(autoError);
+                }
+            }
+            if (Kunde == null)
+            {
+                error.AppendLine("- Kunde ist nicht zugewiesen.");
+            }
+            else
+            {
+                string kundeError = Kunde.Validate();
+                if (!string.IsNullOrEmpty(kundeError))
+                {
+                    error.AppendLine(kundeError);
+                }
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override string ToString()
+            => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
 
     }
 }
